9d13a419b1cc886dd5bc61a3aca40decca49332b
JiheunSeo <104745881+JiheunSeo@users.noreply.github.com>
2024-02-29 11:04:27 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 780 ms, Memory: 119796 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -1,123 +1,136 @@
-import java.sql.Array;
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    //파이어볼 정보 클래스
-    static class meteor{
-        int r, c, m, s, d;
-        public meteor(int r, int c, int m, int s, int d){
-            this.r = r;
-            this.c = c;
-            this.m = m;
-            this.s = s;
-            this.d = d;
-        }
+	public static int N;
+	public static int M;
+	public static int temp;
+	public static int[] dr = { -1, -1, 0, 1, 1, 1, 0, -1 }; // 상 오위 오 오아래 아래 왼아래 왼 왼위
+	public static int[] dc = { 0, 1, 1, 1, 0, -1, -1, -1 };
+	public static List<FireBall>[][] visit;
+	public static List<FireBall> fb;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		temp = Integer.parseInt(st.nextToken());
+//        visit = new ArrayList[N][M];
+//        for(int r=0 ; r<N; r++) {
+//        	for(int c=0; c<N; c++) {
+//        		visit[r][c] = new ArrayList<>();
+//        	}
+//        }
+		fb = new ArrayList<>();
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken()); //행
+			int c = Integer.parseInt(st.nextToken()); //열
+			int m = Integer.parseInt(st.nextToken()); //질량
+			int s = Integer.parseInt(st.nextToken()); //속력
+			int d = Integer.parseInt(st.nextToken()); //방향
+			fb.add(new FireBall(r - 1, c - 1, m, s, d));
+		}
+		
+		//움직여야하는 횟수만큼 움직임
+		for (int i = 1; i <= temp; i++) {
+//        	System.out.println(fb);
+			move();
+		}
+//        System.out.println("결과" + fb);
+		
+		//모든 움직임이 끝난 후 파이어볼의 질량 합 구하기
+		int total = 0;
+		for (int i = 0; i < fb.size(); i++) {
+			total += fb.get(i).m;
+		}
+		System.out.println(total);
+
+	}
+	
+	//이동함수
+	public static void move() {
+		//파이어볼 담을 배열
+		visit = new ArrayList[N][N];
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				visit[r][c] = new ArrayList<>();
+			}
+		}
+		
+		//기존 파이어볼 개수만큼 행, 열 이동 후 visit 배열에 저장
+		for (int i = 0; i < fb.size(); i++) {
+			int r = (fb.get(i).r + N + (fb.get(i).s % N) * dr[fb.get(i).d]) % N;
+			int c = (fb.get(i).c + N + (fb.get(i).s % N) * dc[fb.get(i).d]) % N;
+			visit[r][c].add(new FireBall(r, c, fb.get(i).m, fb.get(i).s, fb.get(i).d));
+		}
+		
+		//기존 파이어볼 리스트 초기화
+		fb.clear();
+		
+		//visit 배열 사이즈 만큼 반복
+		for (int r = 0; r < N; r++) {
+			for (int c = 0; c < N; c++) {
+				//파이어볼이 두개이상일때
+				if (visit[r][c].size() >= 2) {
+					int m = 0; //질량 총합
+					int s = 0; //속력 총합
+					int d = (visit[r][c].get(0).d) % 2; //첫번째 파이어볼 방향 홀짝여부
+					boolean flag = true;
+					for (int i = 0; i < visit[r][c].size(); i++) {
+						m += visit[r][c].get(i).m;
+						s += visit[r][c].get(i).s;
+						if ((visit[r][c].get(i).d) % 2 != d) { // 홀짝 다른 경우
+							flag = false;
+						}
+					}
+					//만약 질량의 합이 4이하인 경우 질량 0되므로 continue;
+					if (m / 5 == 0) {
+						continue;
+					} else if (flag) { //모두 다 홀이거나 짝일때
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 0));
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 2));
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 4));
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 6));
+					} else {
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 1));
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 3));
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 5));
+						fb.add(new FireBall(r, c, m / 5, s / visit[r][c].size(), 7));
+					}
+				//파이어볼이 하나만 있을 때
+				} else if (visit[r][c].size() == 1) {
+					fb.add(visit[r][c].get(0));
+				}
+			}
+		}
+	}
+
+	public static class FireBall {
+		int r;
+		int c;
+		int m; // 질량
+		int s; // 속력
+		int d; // 방향
+
+		public FireBall(int r, int c, int m, int s, int d) {
+			super();
+			this.r = r;
+			this.c = c;
+			this.m = m;
+			this.s = s;
+			this.d = d;
+		}
+
 		@Override
 		public String toString() {
-			return "meteor [r=" + r + ", c=" + c + ", m=" + m + ", s=" + s + ", d=" + d + "]";
+			return "FireBall [r=" + r + ", c=" + c + ", m=" + m + ", s=" + s + ", d=" + d + "]";
 		}
-        
-    }
-    
-    
-    static int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};	//방향 r값 변경값
-    static int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};	//방향 c값 변경값
-    static ArrayList<meteor>[][] map;	//파이어볼 이동했을 때 정보
-    static ArrayList<meteor> meteors = new ArrayList<>();	//모든 파이어볼 정보
-    public static void main(String[] args) throws IOException {
-        //입력값 처리하는 BufferedReader
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //결과값 출력하는 BufferedWriter
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        map = new ArrayList[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++)
-                map[i][j] = new ArrayList<>();
-        }
-        //입력되는 파이어볼 정보 저장
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int r = Integer.parseInt(st.nextToken()) - 1;
-            int c = Integer.parseInt(st.nextToken()) - 1;
-            int m = Integer.parseInt(st.nextToken());
-            int s = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            meteors.add(new meteor(r, c, m, s, d));
-        }
-        //K번 이동명령 진행
-        for (int i = 0; i < K; i++) {
-//        	System.out.println(meteors);
-            meteorMove(N);
-            meteorFire(N);
-        }
-//        System.out.println(meteors);
-        bw.write(meteorCal() + "");	//메테오 질량의 합 BufferedWriter 저장
-        bw.flush();		//결과 출력
-        bw.close();
-        br.close();
-    }
-    //파이어볼 이동시키는 함수
-    static void meteorMove(int N) {
-        for (meteor cur : meteors) {
-            //r, c값 변경
-            // +N을 하는 이유는 이동하였을 때 음수가 나올 수 있기 때문입니다.
-            int tempR = (cur.r + N + dr[cur.d] * (cur.s%N)) % N;
-            int tempC = (cur.c + N + dc[cur.d] * (cur.s%N)) % N;
-            cur.r = tempR;
-            cur.c = tempC;
-            //이동한 파이어볼 저장
-            map[cur.r][cur.c].add(cur);
-        }
-    }
-    //파이어볼 분열 진행
-    static void meteorFire(int N){
-        for(int r = 0; r<N;r++){
-            for(int c = 0; c<N;c++) {
-                //파이어볼 개수가 2개 미만일 때
-                if(map[r][c].size() < 2){
-                    map[r][c].clear();
-                    continue;
-                }
-                //파이어볼 2개 이상일 때
-                int mSum = 0, sSum = 0, oddCount = 0, evenCount = 0;
-                int size = map[r][c].size();
-                //분열 진행!
-                for(meteor cur : map[r][c]){
-                    mSum += cur.m;	//질량 더하기
-                    sSum += cur.s;	//속도 더하기
-                    if(cur.d % 2 == 1)	//방향 홀수일 때
-                        oddCount++;
-                    else		//방향 짝수일 때
-                        evenCount++;
-                    meteors.remove(cur);	//분열될 파이어볼 제거!
-                }
-                map[r][c].clear();
-                mSum /= 5;	//분열될 질량 구하기
-                if(mSum == 0)	//분열될 질량이 0이면 분열 실패!
-                    continue;
-                sSum /= size;	//분열될 속도 구하기
-                //모든 파이어볼 방향이 짝수이거나 홀수일 때
-                if(oddCount == size || evenCount == size){
-                    for(int i=0;i<8;i+=2)	//{0, 2, 4, 6} 방향 분열
-                        meteors.add(new meteor(r,c,mSum, sSum, i));
-                }else{
-                    for(int i=1;i<8;i+=2)	//{1, 3, 5, 7} 방향 분열
-                        meteors.add(new meteor(r,c,mSum, sSum, i));
-                }
-            }
-        }
-    }
-    //파이어볼 질량의 합 구하는 함수
-    static int meteorCal(){
-        int result = 0;
-        //모든 질량 더하기!
-        for(meteor cur : meteors)
-            result += cur.m;
-        return result;
-    }
+
+	}
 }
