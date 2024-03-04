80a102a6f26851e9a26171baa4f4d2f761ebd460
JiheunSeo <104745881+JiheunSeo@users.noreply.github.com>
2024-03-04 21:46:46 +0900

    [Gold III] Title: 양팔저울, Time: 124 ms, Memory: 15244 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.java
+++ 백준/Gold/2629. 양팔저울/양팔저울.java
@@ -4,41 +4,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int[] chu;
 	public static int CHU;
+	public static int[] chu;
 	public static int N;
-	public static boolean [][] memo;
+	public static int[] weight;
+	public static boolean[][] memo;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
 		CHU = Integer.parseInt(br.readLine());
 		chu = new int[CHU+1];
+		
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<CHU; i++) {
+		for(int i=1; i<CHU+1 ; i++) {
 			chu[i] = Integer.parseInt(st.nextToken());
 		}
-		
 		N = Integer.parseInt(br.readLine());
+		weight = new int[N];
 		st = new StringTokenizer(br.readLine());
-		memo = new boolean[CHU+1][15001];
+		for(int i=0; i<N; i++) {
+			weight[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		memo = new boolean[CHU+1][40001];
 		dp(0,0);
-		for(int i=1; i<N+1; i++) {
-			int target = Integer.parseInt(st.nextToken());
-			if(target>15000) sb.append("N ");
-			else if(memo[CHU][target]) sb.append("Y ");
-			else sb.append("N ");
+		for(int i=0; i<N; i++) {
+			if(N>15000) {
+				sb.append("N ");
+			}
+			if(memo[CHU][weight[i]]) {
+				sb.append("Y ");
+			}else {
+				sb.append("N ");
+			}
 		}
 		System.out.println(sb);
 		
 	}
 	public static void dp(int i, int w) {
-		if(i>CHU || memo[i][w]) return;
-//		System.out.println(i + " " + w);
-		memo[i][w] = true;
-		dp(i+1, w + chu[i]); //추 올린경우
-		dp(i+1, w); //추 안올림
-		dp(i+1, Math.abs(w - chu[i])); //추 구슬과 같이 올림
+		
+		if(i<=CHU) {
+			if(memo[i][w]) return;
+			memo[i][w] = true;
+		}
+		if(i>=CHU) return;
+		dp(i+1,w + chu[i+1]);
+		dp(i+1, w);
+		dp(i+1, Math.abs(chu[i+1] - w));
 		
 	}
 }
