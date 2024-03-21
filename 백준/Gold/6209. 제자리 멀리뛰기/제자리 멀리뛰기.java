345b6a6478ea263ef56d0e2865d04454a4eac22e
JiheunSeo <104745881+JiheunSeo@users.noreply.github.com>
2024-03-21 23:05:51 +0900

    [Gold III] Title: 제자리 멀리뛰기, Time: 296 ms, Memory: 22008 KB -BaekjoonHub

--- 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.java
+++ 백준/Gold/6209. 제자리 멀리뛰기/제자리 멀리뛰기.java
@@ -1,71 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+//이분탐색을 이용해 최소거리의 최댓값 찾기
+//초기설정은 거리의 중간값
+//그 값 기준으로 탐색하면서 제거해야할 돌맹이 개수 세기(제거하지 않는다면 거리 idx 설정)
 public class Main {
-
-    private int distance, island, delete;
-    private List<Integer> islands;
-    {
-        islands = new ArrayList<>();
-    }
-
-    public static void main(String[] args){
-        (new Main()).solve();
-    }
-
-    private void solve(){
-        input();
-        System.out.println(parametricSearch());
-    }
-
-    private void input(){
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
-            String[] line = br.readLine().split(" ");
-            distance = Integer.parseInt(line[0]);
-            island = Integer.parseInt(line[1]);
-            delete = Integer.parseInt(line[2]);
-            islands.add(0);
-            for(int i = 0; i < island; i++) islands.add(Integer.parseInt(br.readLine()));
-            islands.add(distance);
-            Collections.sort(islands);
-        }catch(IOException ignored){}
-    }
-
-    private int parametricSearch(){
-        int left = 0, right = distance;
-        int ans = getMinJumpDistance();
-        while(left <= right){
-            int mid = (left + right) / 2;
-            if(isFarable(mid)){
-                left = mid + 1;
-                ans = mid;
-                continue;
-            }
-            right = mid - 1;
-        }
-        return ans;
-    }
-
-    private int getMinJumpDistance(){
-        int ans = distance;
-        for(int i = 1; i < islands.size(); i++) ans = Math.min(ans, islands.get(i) - islands.get(i-1));
-        return ans;
-    }
-
-    private boolean isFarable(int distance){
-        int currentDeleted = delete;
-        int beforeIdx = 0;
-        for(int i = 1; i < islands.size(); i++){
-            if(islands.get(i) - beforeIdx < distance) {
-                if(currentDeleted == 0) return false;
-                currentDeleted--;
-                continue;
-            }
-            beforeIdx = islands.get(i);
-        }
-        return true;
-    }
-
+	public static int D;
+	public static int N;
+	public static int M;
+	public static int[] dist;
+	public static List<Integer> rst = new ArrayList<>();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		D = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		dist = new int[N+2];
+		for(int i=1; i<N+1; i++) {
+			dist[i] = Integer.parseInt(br.readLine());
+		}
+		dist[N+1] = D;
+		Arrays.sort(dist);
+//		System.out.println(Arrays.toString(dist));
+		
+		int firmin = 0;
+//		for(int i=1; i<N+2; i++) {
+//			firmin = Math.min((dist[i] - dist[i-1]), firmin);
+//		}
+//		
+//		//제거할수 있는 돌맹이가 0개일때
+//		if(M==0) {
+//			System.out.println(firmin);
+//			System.exit(0);
+//		}
+		
+		int start = 0;
+		int end = D;
+		
+		while(start<=end) {
+			int idx = 0;
+			int cnt = 0;
+			int mid = (end + start) / 2;
+//			System.out.println("===");
+//			System.out.println(mid + " " + start + " " + end);
+//			System.out.println(mid);
+			for(int i=1; i<N+1; i++) {
+				if(cnt>M) break;
+				if(dist[i] - dist[idx] < mid) {
+					cnt++;
+				} else {
+					idx = i;
+				}
+			}
+//			System.out.println(cnt +" " + idx + " " +N+1);
+//			System.out.println(dist[N+1] + " " + dist[idx]);
+			if(cnt>M) {
+				end = mid - 1;
+			} else {
+				firmin = mid;
+				start = mid+1;
+			}
+//			System.out.println(start + " " + end);
+		}
+		System.out.println(firmin);
+	}
 }
